7ee3cdc9b95211c6f2a81ec75da73732e52767b5
Nattawat Lertchidumrongsri-6809617126 <little0747@gmail.com>
2025-11-22 04:51:56 +0000

    Define $file variable pointing to JSON storage file

--- PHPFiles/save_registration.php
+++ PHPFiles/save_registration.php
@@ -3,3 +3,7 @@ header("Content-Type: application/json");
 
 // รับ JSON จาก JS
 $data = json_decode(file_get_contents("php://input"), true);
+
+//ไฟล์​  JSON ที่เก็บข้อมูล
+$file = "../JSONFiles/registration_data.json";
+
