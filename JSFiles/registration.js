e3839735a52f6641c65623610e9d246f7c31bfba
Nattawat Lertchidumrongsri-6809617126 <little0747@gmail.com>
2025-11-22 04:07:25 +0000

    Send form data to server using fetch API (bug)

--- JSFiles/registration.js
+++ JSFiles/registration.js
@@ -19,4 +19,8 @@ document.getElementById("CafeForm").addEventListener("submit", function(e){
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data)
     })
+    .then(res => res.json())
+    .then(result => {
+        alert(using)
+    });
 });
