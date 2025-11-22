8b45c0234f7e3ce276dcbb022429d8e97264d6a9
Nattawat Lertchidumrongsri-6809617126 <little0747@gmail.com>
2025-11-22 03:43:30 +0000

    Create data object for form submission payload

--- JSFiles/registration.js
+++ JSFiles/registration.js
@@ -1,3 +1,16 @@
 document.getElementById("CafeForm").addEventListener("submit", function(e){
     e.preventDefault();
+
+    let data = {
+        firstname: document.getElementById("firstname").value,
+        lastname: document.getElementById("lastname").value,
+        email: document.getElementById("email").value,
+        phone: document.getElementById("phone").value,
+        usertype: document.getElementById("usertype").value,
+        interest: [
+            document.getElementById("news").checked ? "ข่าวสาร" : null,
+            document.getElementById("event").checked ? "กิจกรรม" : null
+        ].filter(Boolean),
+        message: document.getElementById("message").value
+    };
 });
