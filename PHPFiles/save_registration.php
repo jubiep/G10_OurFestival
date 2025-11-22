022522f43f5d7e49755eea6a2472350f38187d88
Nattawat Lertchidumrongsri-6809617126 <little0747@gmail.com>
2025-11-22 04:59:36 +0000

    Initialize JSON data storage file if it does not exist

--- PHPFiles/save_registration.php
+++ PHPFiles/save_registration.php
@@ -4,6 +4,11 @@ header("Content-Type: application/json");
 // รับ JSON จาก JS
 $data = json_decode(file_get_contents("php://input"), true);
 
-//ไฟล์​  JSON ที่เก็บข้อมูล
+// ไฟล์​  JSON ที่เก็บข้อมูล
 $file = "../JSONFiles/registration_data.json";
+
+// ถ้าไฟล์ยังไม่มีให้สร้าง
+if (!file_exists($file)) {
+    file_put_contents($file, json_encode([], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+}
 
