bf38d23eed20cf673a741ec176f8f3a9c0cee6de
Boom34906 <natchaninpahnichaputt34906@gmail.com>
2025-11-22 19:24:24 +0700

    nav bar: add tooltip

--- JSFiles/basestyle.js
+++ JSFiles/basestyle.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
+
+  // Navbar Toggle
   const navbarToggle = document.getElementById("navbarToggle");
   const navbarMenu = document.getElementById("navbarMenu");
 
@@ -7,4 +9,14 @@ document.addEventListener("DOMContentLoaded", function() {
       navbarMenu.classList.toggle("show");
     });
   }
+
+  // Tooltip
+  const tooltipTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  );
+
+  tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+  });
+
 });
