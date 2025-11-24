a2a6e88abd0e780ec30e857529f909aee7dd4fb5
Nattawat Lertchidumrongsri-6809617126 <little0747@gmail.com>
2025-11-24 13:48:34 +0000

    Fix bug

--- PHPFiles/get_registration.php
+++ PHPFiles/get_registration.php
@@ -5,12 +5,12 @@ $file = "../JSONFiles/registration_data.json";
 
 //ถ้าไฟล์ไม่มี => ส่ง JSON เปล่า
 if (!file_exists($file)) {
-    echo json_decode([], JSON_UNESCAPED_UNICODE);
+    echo json_encode([], JSON_UNESCAPED_UNICODE);
     exit;
 }
 
 // อ่านข้อมูล JSON
-$data = "json_decode(file_get_contents($file), true";
+$data = json_decode(file_get_contents($file), true);
 
 // ส่งข้อมูลออกไปเป็น JSON
 echo json_encode($data, JSON_UNESCAPED_UNICODE);
